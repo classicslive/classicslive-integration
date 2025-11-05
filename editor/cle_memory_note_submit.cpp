8d1512c57ee9677a3a5ac8a410f4911489a6af05
keith <keith@desktop.lan>
2025-11-05 14:48:07 -0600

    hookup memnote submit to new api

--- editor/cle_memory_note_submit.cpp
+++ editor/cle_memory_note_submit.cpp
@@ -1,5 +1,29 @@
 #include "cle_memory_note_submit.h"
 
+extern "C"
+{
+  #include "../cl_json.h"
+}
+
+static CL_NETWORK_CB(cle_memory_note_add_cb)
+{
+  if (!response.data || !userdata)
+    return;
+  else
+  {
+    unsigned memory_note_id;
+
+    if (cl_json_get(&memory_note_id, response.data, "memory_note_id",
+                    CL_JSON_TYPE_NUMBER, sizeof(memory_note_id)))
+    {
+      cl_memnote_t *note = (cl_memnote_t*)userdata;
+      note->key = memory_note_id;
+      cl_memory_add_note(note);
+    }
+    free(userdata);
+  }
+}
+
 CleMemoryNoteSubmit::CleMemoryNoteSubmit(cl_memnote_t note)
 {
   QGridLayout *Layout = new QGridLayout(this);
@@ -50,11 +74,14 @@ void CleMemoryNoteSubmit::onClickSubmit(void)
     return;
   else
   {
+    cl_memnote_t *note;
     QByteArray post;
+
     post.append("game_id=" + QByteArray::number(session.game_id));
     post.append("&address=" + QByteArray::number((qulonglong)m_MemoryNote.address));
     post.append("&type=" + QByteArray::number(m_MemoryNote.type));
 
+    /* Set the pointer parameters */
     if (m_MemoryNote.pointer_passes)
     {
       QStringList offsets;
@@ -63,15 +90,29 @@ void CleMemoryNoteSubmit::onClickSubmit(void)
       post.append("&offsets=" + QUrl::toPercentEncoding(offsets.join(" ")));
     }
 
+    /* Set the title */
     QString title = m_Title->text();
-    QString description = m_Description->toPlainText();
-
+    snprintf(m_MemoryNote.details.title,
+             sizeof(m_MemoryNote.details.title),
+             "%s", title.toStdString().c_str());
     post.append("&title=" + QUrl::toPercentEncoding(title));
 
+    /* Set the description */
+    QString description = m_Description->toPlainText();
     if (!description.isEmpty())
+    {
+      snprintf(m_MemoryNote.details.description,
+               sizeof(m_MemoryNote.details.description),
+               "%s", description.toStdString().c_str());
       post.append("&description=" + QUrl::toPercentEncoding(description));
+    }
 
-    cl_network_post_api(CL_END_MEMORYNOTE_ADD, post.data(), NULL);
+    /* Create a copy of the memory note to send with the network request */
+    note = new cl_memnote_t;
+    *note = m_MemoryNote;
+
+    cl_network_post_api(CL_END_MEMORYNOTE_ADD, post.data(),
+                        cle_memory_note_add_cb, note);
     close();
   }
 }
