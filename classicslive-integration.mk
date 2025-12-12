c3b5bd26ae4000d27019821746d8bfab9bd4260a
celerizer <celerizer@gmail.com>
2025-12-12 16:07:50 -0600

    export cl and 3rdparty source separately

--- classicslive-integration.mk
+++ classicslive-integration.mk
@@ -2,7 +2,7 @@
 CLASSICS_LIVE_DIR ?= $(PWD)
 CLASSICS_LIVE_LIBRETRO_DIR ?= $(PWD)/../libretro-common
 
-CLASSICS_LIVE_SOURCES := \
+CLASSICS_LIVE_SOURCES_CLASSICSLIVE = \
     $(CLASSICS_LIVE_DIR)/cl_action.c \
     $(CLASSICS_LIVE_DIR)/cl_common.c \
     $(CLASSICS_LIVE_DIR)/cl_config.c \
@@ -13,7 +13,9 @@ CLASSICS_LIVE_SOURCES := \
     $(CLASSICS_LIVE_DIR)/cl_memory.c \
     $(CLASSICS_LIVE_DIR)/cl_network.c \
     $(CLASSICS_LIVE_DIR)/cl_script.c \
-    $(CLASSICS_LIVE_DIR)/cl_search.c \
+    $(CLASSICS_LIVE_DIR)/cl_search.c
+
+CLASSICS_LIVE_SOURCES_LIBRETRO = \
     $(CLASSICS_LIVE_LIBRETRO_DIR)/compat/compat_strl.c \
     $(CLASSICS_LIVE_LIBRETRO_DIR)/compat/fopen_utf8.c \
     $(CLASSICS_LIVE_LIBRETRO_DIR)/encodings/encoding_base64.c \
@@ -31,7 +33,7 @@ CLASSICS_LIVE_SOURCES := \
     $(CLASSICS_LIVE_LIBRETRO_DIR)/vfs/vfs_implementation.c
 
 ifeq ($(CL_HAVE_FILESYSTEM),1)
-CLASSICS_LIVE_SOURCES += \
+CLASSICS_LIVE_SOURCES_LIBRETRO += \
     $(CLASSICS_LIVE_LIBRETRO_DIR)/streams/chd_stream.c \
     $(CLASSICS_LIVE_LIBRETRO_DIR)/formats/libchdr/libchdr_bitstream.c \
     $(CLASSICS_LIVE_LIBRETRO_DIR)/formats/libchdr/libchdr_cdrom.c \
@@ -39,3 +41,7 @@ CLASSICS_LIVE_SOURCES += \
     $(CLASSICS_LIVE_LIBRETRO_DIR)/formats/libchdr/libchdr_huffman.c \
     $(CLASSICS_LIVE_LIBRETRO_DIR)/streams/interface_stream.c
 endif
+
+CLASSICS_LIVE_SOURCES = \
+    $(CLASSICS_LIVE_SOURCES_CLASSICSLIVE) \
+    $(CLASSICS_LIVE_SOURCES_LIBRETRO)
