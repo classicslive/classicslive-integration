35a41f881ce5c2ebf53bdd6c45047bd1cddd8dfb
Keith Bourdon <33245078+celerizer@users.noreply.github.com>
2025-12-17 20:37:00 -0600

    remove the old search

--- cl_search.c
+++ cl_search.c
@@ -7,197 +7,19 @@
 #include <math.h>
 #include <string.h>
 
-cl_searchbank_t* cl_searchbank_from_address(cl_search_t *search, 
-  cl_addr_t address)
-{
-  if (!search)
-    return NULL;
-  else
-  {
-    cl_searchbank_t *sbank;
-    uint8_t i;
-
-    for (i = 0; i < search->searchbank_count; i++)
-    {
-      sbank = &search->searchbanks[i];
-      if (sbank->region->base_guest <= address &&
-          sbank->region->base_guest + sbank->region->size > address)
-      return sbank;
-    }
-
-    return NULL;
-  }
-}
-
-#if CL_EXTERNAL_MEMORY
-cl_error cl_search_deep_copy(cl_search_t *search)
-{
-  unsigned i;
-
-  if (!search)
-    return CL_ERR_PARAMETER_NULL;
-  else for (i = 0; i < search->searchbank_count; i++)
-  {
-    cl_searchbank_t *sbank = &search->searchbanks[i];
-
-    if (!sbank->region->base_host)
-      continue;
-    else
-      cl_abi_external_read(sbank->region->base_host + sbank->first_valid,
-        sbank->region->base_guest + sbank->first_valid,
-        sbank->last_valid - sbank->first_valid + search->params.size,
-        NULL
-      );
-  }
-
-  return CL_OK;
-}
-#endif
-
-bool cl_search_free(cl_search_t *search)
-{
-  if (!search)
-    return false;
-  else
-  {
-    uint8_t i;
-
-    for (i = 0; i < search->searchbank_count; i++)
-    {
-      free(search->searchbanks[i].backup);
-      free(search->searchbanks[i].valid);
-#if CL_EXTERNAL_MEMORY
-      free(search->searchbanks[i].region->base_host);
-#endif
-    }
-    free(search->searchbanks);
-
-    return true;
-  }
-}
-
-bool cl_search_init(cl_search_t *search)
-{
-  if (!memory.region_count)
-    return false;
-  else
-  {
-    uint8_t i;
-
-    cl_log("Initializing a new search...\n");
-    search->matches = 0;
-    search->searchbank_count = memory.region_count;
-    search->searchbanks = (cl_searchbank_t*)calloc(search->searchbank_count, sizeof(cl_searchbank_t));
-
-    search->params.compare_type = CLE_CMPTYPE_EQUAL;
-    search->params.size = 1;
-    search->params.value_type = CL_MEMTYPE_UINT8;
-
-    for (i = 0; i < search->searchbank_count; i++)
-    {
-      cl_searchbank_t *sbank = &search->searchbanks[i];
-
-      sbank->any_valid = true;
-      sbank->region = &memory.regions[i];
-#if CL_EXTERNAL_MEMORY
-      sbank->region->base_host = malloc(memory.regions[i].size);
-#endif
-      sbank->backup = (uint8_t*)malloc(memory.regions[i].size);
-      sbank->valid = (uint8_t*)malloc(memory.regions[i].size);
-    }
-    cl_search_reset(search);
-  }
-
-  return true;
-}
-
-bool cl_read_search(uint32_t *value, cl_search_t *search, cl_searchbank_t *sbank, cl_addr_t address)
-{
-  if (!sbank)
-  {
-    if (memory.region_count == 0)
-      return false;
-    else if (memory.region_count == 1)
-    {
-      sbank = &search->searchbanks[0];
-      address -= sbank->region->base_guest;
-    }
-    else
-    {
-      unsigned i;
-
-      for (i = 0; i < memory.region_count; i++)
-      {
-        if (address >= memory.regions[i].base_guest &&
-            address < memory.regions[i].base_guest + memory.regions[i].size)
-        {
-          sbank = &search->searchbanks[i];
-          address -= sbank->region->base_guest;
-        }
-      }
-    }
-  }
-  if (sbank->backup)
-    return cl_read(value, sbank->backup, address, search->params.size, sbank->region->endianness);
-
-  return false;
-}
-
-bool cl_search_remove(cl_search_t *search, cl_addr_t address)
-{
-  cl_searchbank_t *sbank = cl_searchbank_from_address(search, address);
-
-  if (!sbank || sbank->valid[address -   sbank->region->base_guest] == 0)
-    return false;
-  else
-  {
-    sbank->valid[address -   sbank->region->base_guest] = 0;
-    search->matches--;
-
-    return true;
-  }
-}
-
-bool cl_search_reset(cl_search_t *search)
-{
-  if (!search)
-    return false;
-  else
-  {
-    cl_searchbank_t *sbank;
-    uint8_t i;
-
-#if CL_EXTERNAL_MEMORY
-    cl_search_deep_copy(search);
-#endif
-    for (i = 0; i < search->searchbank_count; i++)
-    {
-      sbank = &search->searchbanks[i];
-      memcpy(sbank->backup, sbank->region->base_host, sbank->region->size);
-      memset(sbank->valid, 1, sbank->region->size);
-      sbank->any_valid = true;
-      sbank->first_valid = 0;
-      sbank->last_valid = sbank->region->size - 1;
-    }
-    search->matches = 0;
-
-    return true;
-  }
-}
-
 bool compare_to_nothing(uint32_t previous, uint32_t current, uint8_t type)
 {
   switch (type)
   {
-  case CLE_CMPTYPE_EQUAL:
+  case CL_COMPARE_EQUAL:
     return previous == current;
-  case CLE_CMPTYPE_LESS:
-  case CLE_CMPTYPE_DECREASED:
+  case CL_COMPARE_LESS:
+  case CL_COMPARE_DECREASED:
     return previous > current;
-  case CLE_CMPTYPE_GREATER:
-  case CLE_CMPTYPE_INCREASED:
+  case CL_COMPARE_GREATER:
+  case CL_COMPARE_INCREASED:
     return previous < current;
-  case CLE_CMPTYPE_NOT_EQUAL:
+  case CL_COMPARE_NOT_EQUAL:
     return previous != current;
   }
 
@@ -214,15 +36,15 @@ bool compare_to_nothing_float(uint32_t previous, uint32_t current, uint8_t type)
 
   switch (type)
   {
-  case CLE_CMPTYPE_EQUAL:
+  case CL_COMPARE_EQUAL:
     return (uint32_t)fprevious == (uint32_t)fcurrent;
-  case CLE_CMPTYPE_LESS:
-  case CLE_CMPTYPE_DECREASED:
+  case CL_COMPARE_LESS:
+  case CL_COMPARE_DECREASED:
     return (uint32_t)fprevious > (uint32_t)fcurrent;
-  case CLE_CMPTYPE_GREATER:
-  case CLE_CMPTYPE_INCREASED:
+  case CL_COMPARE_GREATER:
+  case CL_COMPARE_INCREASED:
     return (uint32_t)fprevious < (uint32_t)fcurrent;
-  case CLE_CMPTYPE_NOT_EQUAL:
+  case CL_COMPARE_NOT_EQUAL:
     return (uint32_t)fprevious != (uint32_t)fcurrent;
   }
 
@@ -233,17 +55,17 @@ bool compare_to_value(uint32_t previous, uint32_t current, uint8_t type, uint32_
 {
   switch (type)
   {
-  case CLE_CMPTYPE_EQUAL:
+  case CL_COMPARE_EQUAL:
     return current == value;
-  case CLE_CMPTYPE_GREATER:
+  case CL_COMPARE_GREATER:
     return current > value;
-  case CLE_CMPTYPE_LESS:
+  case CL_COMPARE_LESS:
     return current < value;
-  case CLE_CMPTYPE_NOT_EQUAL:
+  case CL_COMPARE_NOT_EQUAL:
     return current != value;
-  case CLE_CMPTYPE_INCREASED:
+  case CL_COMPARE_INCREASED:
     return current == previous + value;
-  case CLE_CMPTYPE_DECREASED:
+  case CL_COMPARE_DECREASED:
     return current + value == previous;
   }
 
@@ -269,23 +91,23 @@ bool compare_to_value_float(uint32_t previous, uint32_t current, uint8_t type,
 
   switch (type)
   {
-  case CLE_CMPTYPE_EQUAL:
+  case CL_COMPARE_EQUAL:
     if (has_decimal_precision)
       return fcurrent == value;
     else
       return floor(fcurrent) == value;
-  case CLE_CMPTYPE_GREATER:
+  case CL_COMPARE_GREATER:
     return fcurrent > value;
-  case CLE_CMPTYPE_LESS:
+  case CL_COMPARE_LESS:
     return fcurrent < value;
-  case CLE_CMPTYPE_NOT_EQUAL:
+  case CL_COMPARE_NOT_EQUAL:
     return fcurrent != value;
-  case CLE_CMPTYPE_INCREASED:
+  case CL_COMPARE_INCREASED:
     if (has_decimal_precision)
       return fcurrent == fprevious + value;
     else
       return floor(fcurrent) == floor(fprevious) + value;
-  case CLE_CMPTYPE_DECREASED:
+  case CL_COMPARE_DECREASED:
     if (has_decimal_precision)
       return fcurrent + value == fprevious;
     else
@@ -315,148 +137,6 @@ bool resolve_pointerresult(cl_addr_t *final_address, const cl_pointerresult_t *r
   *final_address = address;
 
   return true;
-}
-
-uint32_t cl_search_ascii(cl_search_t *search, const char *needle, uint8_t length)
-{
-  if (!search || search->searchbank_count == 0)
-    return 0;
-  else
-  {
-    cl_searchbank_t *sbank;
-    const char *haystack;
-    uint32_t matches = 0;
-    uint8_t  i;
-    uint32_t j;
-
-    for (i = 0; i < search->searchbank_count; i++)
-    {
-      uint32_t matches_this_bank = 0;
-
-      sbank = &search->searchbanks[i];
-      memset(sbank->valid, 0,   sbank->region->size);
-      haystack = (const char*)  sbank->region->base_host;
-
-      for (j = 0; j <   sbank->region->size; j++)
-      {
-        if (!memcmp(&haystack[j], needle, length))
-        {
-          sbank->valid[j] = true;
-          matches_this_bank++;
-        }
-      }
-
-      if (matches_this_bank == 0)
-        sbank->any_valid = false;
-      else
-        matches += matches_this_bank;
-      memcpy(sbank->backup, sbank->region->base_host, sbank->region->size);
-    }
-    search->matches = matches;
-
-    return matches;
-  }
-}
-
-uint32_t cl_search_step(cl_search_t *search, void *value)
-{
-  if (!search || search->searchbank_count == 0)
-    return 0;
-  else
-  {
-    cl_searchbank_t *sbank;
-    bool compare_result;
-    uint32_t left = 0;
-    uint32_t right = 0;
-    uint32_t matches = 0;
-    uint8_t  cmp_type = search->params.compare_type;
-    uint8_t  size    = search->params.size;
-    uint8_t  val_type = search->params.value_type;
-    uint8_t  i;
-    cl_addr_t j;
-
-    if (!value)
-      cl_log("Comparing to nothing...");
-    else if (val_type == CL_MEMTYPE_FLOAT)
-      cl_log("Comparing to %f...", *((float*)value));
-    else
-      cl_log("Comparing to %u...", *((uint32_t*)value));
-
-#if CL_EXTERNAL_MEMORY
-    cl_search_deep_copy(search);
-#endif
-
-    for (i = 0; i < search->searchbank_count; i++)
-    {
-      cl_addr_t matches_this_bank = 0;
-      cl_addr_t last_valid = 0;
-      bool first_found = false;
-
-      sbank = &search->searchbanks[i];
-      if (!sbank->any_valid)
-        continue;
-
-      for (j = sbank->first_valid; j <= sbank->last_valid; j += size)
-      {
-        /* This address has been weeded out already */
-        if (!sbank->valid[j])
-          continue;
-
-        cl_read_search(&left, search, sbank, j);
-        cl_read_memory_internal(&right, NULL, j + sbank->region->base_guest, size);
-
-        if (!value)
-        {
-          if (val_type == CL_MEMTYPE_FLOAT)
-            compare_result = compare_to_nothing_float(left, right, cmp_type);
-          else
-            compare_result = compare_to_nothing(left, right, cmp_type);
-        }
-        else
-        {
-          if (val_type == CL_MEMTYPE_FLOAT)
-            compare_result = compare_to_value_float(left, right, cmp_type, *((float*)value));
-          else
-          {
-            uint32_t* intval = (uint32_t*)value;
-
-            if (size == 1)
-              *intval &= 0xFF;
-            else if (size == 2)
-              *intval &= 0xFFFF;
-
-            compare_result = compare_to_value(left, right, cmp_type, *intval);
-          }
-        }
-
-        if (!compare_result)
-          sbank->valid[j] = 0;
-        else
-        {
-          /* Set our new first valid offset */
-          if (!first_found)
-          {
-            sbank->first_valid = j;
-            first_found = true;
-          }
-          last_valid = j;
-          sbank->valid[j] = 1;
-          matches_this_bank++;
-        }
-      }
-
-      if (matches_this_bank == 0)
-        sbank->any_valid = false;
-      else
-        matches += matches_this_bank;
-      memcpy(sbank->backup, sbank->region->base_host, sbank->region->size);
-      sbank->last_valid = last_valid;
-    }
-    search->matches = matches;
-    cl_log(" %u matches.\n", matches);
-
-    return matches;
-  }
 }
 
 bool cl_pointersearch_free(cl_pointersearch_t *search)
@@ -552,7 +232,7 @@ bool cl_pointersearch_init(cl_pointersearch_t *search,
     /* Initialize search parameters */
     search->passes          = 1;
     search->range          = range;
-    search->params.compare_type = CLE_CMPTYPE_EQUAL;
+    search->params.compare_type = CL_COMPARE_EQUAL;
     search->params.size      = cl_sizeof_memtype(val_type);
     search->params.value_type  = val_type;
 
